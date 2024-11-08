5c8b780ffb76259954053ba30f701b2a3d3f7aae
ying1488 <yingecteach@gmail.com>
2024-11-08 22:07:59 +1100

    .

---
+++ src/Chicken.jsx
@@ -0,0 +1,7 @@
+import "./Chicken.css";
+
+function Chicken(){
+  return <p className="Chicken">cluck Cluck Cluck</p>;
+}
+
+export default Chicken;
---
+++ src/Greeter.jsx
@@ -0,0 +1,3 @@
+export default function Greeter(person){
+  return<h1>Hi there, {props.person}!!!</h1>;
+}
