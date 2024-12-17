186393c3fffe373fc4cac787fe313570df26224c
ying1488 <yingecteach@gmail.com>
2024-12-17 22:55:14 +1100

    score keeper exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
 import ColorBox from "./Colorbox";
 import ColorBoxGrid from "./ColorBoxGrid";
 import Dumbo from "./dumbo";
+import ScoreKeeper from "./scoreKeeper";
 
 function App() {
-  // const colors = [
-  //   "#E53935",
-  //   "#E91E63",
-  //   "#9C27B0",
-  //   "#673AB7",
-  //   "#3F51B5",
-  //   "#2196F3",
-  //   "#03A9F4",
-  //   "#00BCD4",
-  //   "#009688",
-  //   "#4CAF50",
-  //   "#8BC34A",
-  //   "#CDDC39",
-  //   "#FFEB3B",
-  //   "#FFC107",
-  //   "#FF9800",
-  //   "#FF5722",
-  // ];
+  
   
 return(
 <div>
-{/* <ColorBoxGrid colors={colors}/> */}
-<Dumbo/>
+<ScoreKeeper/>
 </div>
 );
 }
